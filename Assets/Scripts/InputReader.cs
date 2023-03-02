4041b601b2a42a927bee452cb74321c1f4af9459
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-03-02 16:22:02 +0200

    Created movement for the character

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,30 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class InputReader : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+
+        private float _horizontalDirection;
+        private float _verticalDirection;
+
+        public void Update()
+        {
+            _horizontalDirection = Input.GetAxisRaw("Horizontal");
+            _verticalDirection = Input.GetAxisRaw("Vertical");
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                _playerEntity.Jump();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            _playerEntity.MoveHorizontally(_horizontalDirection);
+            _playerEntity.MoveVertically(_verticalDirection);
+        }
+    }
+}
